772b50f87400e7040875d505271bb065066715dc
Stanlin Joseph <smjoseph11@DESKTOP-BMT5G8P.localdomain>
2019-06-12 14:16:11 -0500

    added gulpfile for ease of use. troubleshooted npm bc it wasn't recognizing gulp command. problem was in ~/.profile where npm was pointing to a stale module folder instead of the one currently in use

--- public_html/manipulate.js
+++ public_html/manipulate.js
@@ -2,7 +2,7 @@ const sentence = document.querySelector('p');
 console.log(sentence);
 sentence_arr= sentence.innerHTML.split(RegExp("\\s"));
 console.log(sentence_arr);
-function onOverFunction(){
+function onClickFunction(){
   document.getElementById("sentence").innerHTML = "a man jumped on the platform to catch the train"
 }
 
