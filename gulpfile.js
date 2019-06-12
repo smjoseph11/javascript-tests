e2edd681128f625574e6dfa3df2523b3b4c6c518
Stanlin Joseph <smjoseph11@DESKTOP-BMT5G8P.localdomain>
2019-06-12 18:06:22 -0500

    trying to get node and apache to play together via proxy commands on apache server. Still no success...

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
-function defaultTask(cb){
-  cb();
-}
-
-exports.default = defaultTask
+const {watch} = require('gulp');
+const url = require('url');
+const net = require('net');
+const server = net.createServer((c) => {
+  //'connection listener
+  console.log('client connected');
+  c.on('end', ()=> {
+    console.log('client disconnected');
+  });
+  c.write('hello\r\n');
+  c.pipe(c);
+});
+server.on('error', (err)=>{
+  throw err;
+});
+server.listen(3001, ()=>{
+  console.log("listening to port 3001");
+});
+exports.default = function(){
+  watch('public_html/*', {ignoreInitial:false}, function(cb){
+  });
+};
