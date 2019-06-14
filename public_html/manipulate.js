c5e868995b423d59b8699f605f049d86b99c8c4f
Stanlin Joseph <smjoseph11@DESKTOP-BMT5G8P.localdomain>
2019-06-14 12:25:06 -0500

    completed some javascript exercises from http://eloquentjavascript.net/02_program_structure.html

--- public_html/manipulate.js
+++ public_html/manipulate.js
@@ -1,8 +1,29 @@
-const sentence = document.querySelector('p');
-console.log(sentence);
-sentence_arr= sentence.innerHTML.split(RegExp("\\s"));
-console.log(sentence_arr);
-function onClickFunction(){
-  document.getElementById("sentence").innerHTML = "a man jumped on the platform to catch the train"
+//Exercise 2
+console.log("Exercise 2 results")
+for(let i=1;i<101;i++){
+  if(i%3==0 && i%5==0)
+    console.log('FizzBuzz');
+  else if(i%3==0)
+    console.log('Fizz');
+  else if(i%5==0)
+    console.log('Buzz');
+  else
+    console.log(i);
 }
-
+console.log('\n');
+//Exercise 3
+console.log("Exercise 3 results")
+const size=21
+var str='';
+for(let i=0;i<size;i++){
+  for(let j=0;j<size;j++){
+    if((j+i)%2==0){
+      str+=' ';
+    } 
+    else{
+      str+='#'; 
+    }
+  }
+  str+='\n';
+}
+console.log(str);
