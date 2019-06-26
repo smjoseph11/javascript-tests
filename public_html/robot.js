33f218b495cb3be6b90dc93ce273f312876bbacd
Stanlin Joseph <smjoseph11@gmail.com>
2019-06-25 22:09:42 -0500

    more robot methods

--- public_html/robot.js
+++ public_html/robot.js
@@ -41,3 +41,40 @@ class VillageState{
     }
   }
 }
+let first = new VillageState(
+  "Post Office",
+  [{place:"Post Office", address: "Alice's House"}]
+);
+function randomPick(array) {
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+
+function randomRobot(state) {
+  return {direction: randomPick(roadGraph[state.place])};
+}
+function runRobot(state, robot, memory) {
+  for (let turn = 0;; turn++) {
+    if (state.parcels.length == 0) {
+      console.log(`Done in ${turn} turns`);
+      break;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+  }
+}
+VillageState.random = function(parcelCount = 5) {
+  let parcels = [];
+  for (let i = 0; i < parcelCount; i++) {
+    let address = randomPick(Object.keys(roadGraph));
+    let place;
+    do {
+      place = randomPick(Object.keys(roadGraph));
+    } while (place == address);
+    parcels.push({place, address});
+  }
+  return new VillageState("Post Office", parcels);
+};
+runRobot(VillageState.random(), randomRobot);
